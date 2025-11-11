176f58c1e35df32901162f0ff8dfcc5756f0ed69
labub-bit <118575330+labub-bit@users.noreply.github.com>
2025-11-11 17:56:26 +0800

    Add files via upload

---
+++ WindowFormsSqlSample/Users.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Users;
+USE Users;
+
+SHOW DATABASES;
+
+CREATE TABLE Users (
+	UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    UserPassword VARCHAR(255) NOT NULL
+);
+
+-- Test accounts
+INSERT INTO Users (Username, UserPassword) VALUES ('admin', '1234');
+INSERT INTO Users (Username, UserPassword) VALUES ('student', 'abcd');
+INSERT INTO Users (Username, UserPassword) VALUES ('dangate', 'cute');
+INSERT INTO Users (Username, UserPassword) VALUES ('billgwapokatalagakainiskangtaoka','joke');
+INSERT INTO Users (Username, UserPassword) VALUES ('cutiecakes','joke');
+INSERT INTO Users (Username, UserPassword) VALUES ('eriesgunagwapo','totoo');
+INSERT INTO Users (Username, UserPassword) VALUES ('hahaha','rawr');
+
+
+
+SELECT * FROM Users;
+
+
+DROP TABLE Users;
+DROP DATABASE Users;
